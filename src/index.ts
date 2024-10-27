0315cec477b0afe0bcffdc2fe89c2afa2720f6ca
ZennyFZ <nhandinhdo291@gmail.com>
2024-10-27 21:40:35 +0700

    update project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-import Elysia from "elysia";
-import { app } from './app'
+import { Elysia, redirect } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { user, note } from './route'
 
-export const server = new Elysia()
-.use(app)
+const app = new Elysia()
+  .use(swagger({
+    documentation: {
+      info: {
+        title: 'Scalar: Modern Swagger UI',
+        version: '1.0.0'
+      },
+      tags: [
+        { name: 'USER', description: 'USER endpoints' },
+        { name: 'NOTE', description: 'NOTE endpoints' }
+      ],
+    },
+    exclude: ['/'],
+    autoDarkMode: true,
+  }))
+  .onError(({ error, code }) => {
+    if (code === 'NOT_FOUND') return 'Not Found :('
+
+    console.error(error)
+  })
+  .get('/', redirect('/swagger'))
+  .use(user)
+  .use(note)
+  .listen(3000)
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
